de5b6804e9d6e83590d12a0243df813e3120c0e0
Nguyễn Võ Hoàng Phi <phinvh@vuthao.local>
2022-11-30 14:40:43 +0700

    fix

--- js/modules/dropdownNavbarModule.js
+++ js/modules/dropdownNavbarModule.js
@@ -1,14 +1,14 @@
 export default function dropdownNavbarModule() {
-  $(".header-navbar-search-input").focus(function () {
-    $("#header-search").addClass("move");
-  });
-  $(".header-navbar-search-input").focusout(function () {
-    $("#header-search").removeClass("move");
-    $(".header-navbar-search-input").val("");
-  });
+  // $(".header-navbar-search-input").focus(function () {
+  //   $("#header-search").addClass("move");
+  // });
+  // $(".header-navbar-search-input").focusout(function () {
+  //   $("#header-search").removeClass("move");
+  //   $(".header-navbar-search-input").val("");
+  // });
 
-  $(".ic-search").click(function () {
-    $(".header-navbar-search-input").toggleClass("active");
-    $("#header-search").toggleClass("active");
-  });
+  // $(".ic-search").click(function () {
+  //   $(".header-navbar-search-input").toggleClass("active");
+  //   $("#header-search").toggleClass("active");
+  // });
 }
